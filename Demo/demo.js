5c76dc17d3ce2b4942d5592c28d37ecc7ccfa8da
JadHajjar <jad@brains-lb.com>
2024-01-22 11:59:23 +0200

    Update demo design

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,13 +1,23 @@
 function showTab(tabId) {
-    // Hide all tabs
-    var tabs = document.querySelectorAll('.tab-content');
-    tabs.forEach(function (tab) {
-      tab.classList.remove('active');
-    });
+  // Hide all tabs
+  var tabs = document.querySelectorAll('.tab');
+  var tabContents = document.querySelectorAll('.tab-content');
+  tabContents.forEach(function (tab) {
+    tab.classList.remove('active');
+  });
+  tabs.forEach(function (tab) {
+    if (tab.id === ('tab-' + tabId))
+      tab.classList.add('selected');
+    else
+      tab.classList.remove('selected');
+  });
 
-     // Show the selected tab
+  // Show the selected tab
   var selectedTab = document.getElementById(tabId);
   selectedTab.classList.add('active');
+
+  window.scrollTo(0, 0);
+  return;
 
   // Get the section element
   var section = document.querySelector('section');
